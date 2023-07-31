f82c02ed520b30e286259e73396f12322a292693
Aaron Pavlick <apavlick@yext.com>
2023-07-31 15:03:18 -0400

    temporarily comment out the doctor page

--- src/templates/doctor.tsx
+++ src/templates/doctor.tsx
@@ -25,153 +25,157 @@ import Reviews from "../components/Reviews";
 import Faqs from "../components/Faqs";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-export const config: TemplateConfig = {
-  stream: {
-    $id: "doctors",
-    localization: { locales: ["en"], primary: false },
-    fields: [
-      "id",
-      "name",
-      "slug",
-      "npi",
-      "c_description",
-      "headshot",
-      "c_specialty.name",
-      "c_locationsPracticingAt.address",
-      "c_locationsPracticingAt.geocodedCoordinate",
-      "c_insurances",
-      "c_education",
-      "c_boardCertifications",
-      "c_practiceNames",
-      "c_languagesSpoken",
-      "c_gender",
-      "c_faqs.question",
-      "c_faqs.answer",
-    ],
-    filter: { entityTypes: ["ce_doctor"] },
-  },
-};
-export const getPath: GetPath<TemplateProps> = ({ document }) => {
-  const doctor = document as Doctor;
-  return `${doctor.slug}`;
-};
-
-export const getHeadConfig: GetHeadConfig<TemplateRenderProps> = ({
-  document,
-}): HeadConfig => {
-  const doctor = document as Doctor;
-  return {
-    title: doctor.name,
-    charset: "UTF-8",
-    viewport: "width=device-width, initial-scale=1",
+// export const config: TemplateConfig = {
+//   stream: {
+//     $id: "doctors",
+//     localization: { locales: ["en"], primary: false },
+//     fields: [
+//       "id",
+//       "name",
+//       "slug",
+//       "npi",
+//       "c_description",
+//       "headshot",
+//       "c_specialty.name",
+//       "c_locationsPracticingAt.address",
+//       "c_locationsPracticingAt.geocodedCoordinate",
+//       "c_insurances",
+//       "c_education",
+//       "c_boardCertifications",
+//       "c_practiceNames",
+//       "c_languagesSpoken",
+//       "c_gender",
+//       "c_faqs.question",
+//       "c_faqs.answer",
+//     ],
+//     filter: { entityTypes: ["ce_doctor"] },
+//   },
+// };
+export const getPath: GetPath<TemplateProps> = () =>
+  // { document }
+  {
+    // const doctor = document as Doctor;
+    // return `${doctor.slug}`;
+    return "temp";
   };
-};
 
-const queryClient = new QueryClient();
+// export const getHeadConfig: GetHeadConfig<TemplateRenderProps> = ({
+//   document,
+// }): HeadConfig => {
+//   const doctor = document as Doctor;
+//   return {
+//     title: doctor.name,
+//     charset: "UTF-8",
+//     viewport: "width=device-width, initial-scale=1",
+//   };
+// };
+
+// const queryClient = new QueryClient();
 
 const DoctorPage: Template<TemplateRenderProps> = ({
   document,
 }: TemplateProps) => {
   return (
-    <QueryClientProvider client={queryClient}>
-      <PageLayout>
-        <CenteredContainer classname="max-w-5xl">
-          <Section>
-            <ResponsiveStack className="gap-x-6">
-              <DoctorCard
-                headshot={document.headshot}
-                name={document.name}
-                specialty={document.c_specialty[0].name}
-                rating={4.5}
-                // containerClassname="pr-6"
-              />
-              <DoctorMiniMap locations={document.c_locationsPracticingAt} />
-            </ResponsiveStack>
-          </Section>
-          <Section>
-            <AppointmentGrid />
-          </Section>
-        </CenteredContainer>
+    <></>
+    // <QueryClientProvider client={queryClient}>
+    //   <PageLayout>
+    //     <CenteredContainer classname="max-w-5xl">
+    //       <Section>
+    //         <ResponsiveStack className="gap-x-6">
+    //           <DoctorCard
+    //             headshot={document.headshot}
+    //             name={document.name}
+    //             specialty={document.c_specialty[0].name}
+    //             rating={4.5}
+    //             // containerClassname="pr-6"
+    //           />
+    //           <DoctorMiniMap locations={document.c_locationsPracticingAt} />
+    //         </ResponsiveStack>
+    //       </Section>
+    //       <Section>
+    //         <AppointmentGrid />
+    //       </Section>
+    //     </CenteredContainer>
 
-        <ScrollableContainer>
-          <ScrollableSection
-            title="About"
-            outerContainerClassname="scroll-mt-[166px]"
-          >
-            <BodyText
-              text={document.c_description}
-              className="whitespace-pre-line"
-            />
-          </ScrollableSection>
-          <ScrollableSection
-            title="Education & Background"
-            outerContainerClassname="bg-light-green scroll-mt-24"
-            innerContainerClassname="max-w-5xl"
-          >
-            <DoctorBackground
-              items={[
-                {
-                  icon: "school",
-                  name: "Education",
-                  details: document.c_education,
-                },
-                {
-                  icon: "file-certificate",
-                  name: "Board Certifications",
-                  details: document.c_boardCertifications,
-                },
-                {
-                  icon: "hospital",
-                  name: "Practice Names",
-                  details: document.c_practiceNames,
-                },
-                {
-                  icon: "stethoscope",
-                  name: "Specialties",
-                  details: [document.c_specialty?.[0].name],
-                },
-                {
-                  icon: "language",
-                  name: "Languages Spoken",
-                  details: document.c_languagesSpoken,
-                },
-                {
-                  icon: "venus-mars",
-                  name: "Gender",
-                  details: [document.c_gender],
-                },
-                {
-                  icon: "hashtag",
-                  name: "NPI Number",
-                  details: [document.npi],
-                },
-              ]}
-            />
-          </ScrollableSection>
+    //     <ScrollableContainer>
+    //       <ScrollableSection
+    //         title="About"
+    //         outerContainerClassname="scroll-mt-[166px]"
+    //       >
+    //         <BodyText
+    //           text={document.c_description}
+    //           className="whitespace-pre-line"
+    //         />
+    //       </ScrollableSection>
+    //       <ScrollableSection
+    //         title="Education & Background"
+    //         outerContainerClassname="bg-light-green scroll-mt-24"
+    //         innerContainerClassname="max-w-5xl"
+    //       >
+    //         <DoctorBackground
+    //           items={[
+    //             {
+    //               icon: "school",
+    //               name: "Education",
+    //               details: document.c_education,
+    //             },
+    //             {
+    //               icon: "file-certificate",
+    //               name: "Board Certifications",
+    //               details: document.c_boardCertifications,
+    //             },
+    //             {
+    //               icon: "hospital",
+    //               name: "Practice Names",
+    //               details: document.c_practiceNames,
+    //             },
+    //             {
+    //               icon: "stethoscope",
+    //               name: "Specialties",
+    //               details: [document.c_specialty?.[0].name],
+    //             },
+    //             {
+    //               icon: "language",
+    //               name: "Languages Spoken",
+    //               details: document.c_languagesSpoken,
+    //             },
+    //             {
+    //               icon: "venus-mars",
+    //               name: "Gender",
+    //               details: [document.c_gender],
+    //             },
+    //             {
+    //               icon: "hashtag",
+    //               name: "NPI Number",
+    //               details: [document.npi],
+    //             },
+    //           ]}
+    //         />
+    //       </ScrollableSection>
 
-          <ScrollableSection
-            title="Reviews"
-            outerContainerClassname="scroll-mt-[166px]"
-          >
-            <Reviews entityId={document.id} />
-          </ScrollableSection>
-          <ScrollableSection
-            title="Insurances Accepted"
-            outerContainerClassname="bg-light-green scroll-mt-[166px]]"
-            innerContainerClassname="max-w-5xl"
-          >
-            <Insurances insurances={document.c_insurances} />
-          </ScrollableSection>
-          <ScrollableSection
-            title="FAQs"
-            innerContainerClassname="max-w-5xl"
-            outerContainerClassname="scroll-mt-[166px]"
-          >
-            <Faqs faqs={document.c_faqs} />
-          </ScrollableSection>
-        </ScrollableContainer>
-      </PageLayout>
-    </QueryClientProvider>
+    //       <ScrollableSection
+    //         title="Reviews"
+    //         outerContainerClassname="scroll-mt-[166px]"
+    //       >
+    //         <Reviews entityId={document.id} />
+    //       </ScrollableSection>
+    //       <ScrollableSection
+    //         title="Insurances Accepted"
+    //         outerContainerClassname="bg-light-green scroll-mt-[166px]]"
+    //         innerContainerClassname="max-w-5xl"
+    //       >
+    //         <Insurances insurances={document.c_insurances} />
+    //       </ScrollableSection>
+    //       <ScrollableSection
+    //         title="FAQs"
+    //         innerContainerClassname="max-w-5xl"
+    //         outerContainerClassname="scroll-mt-[166px]"
+    //       >
+    //         <Faqs faqs={document.c_faqs} />
+    //       </ScrollableSection>
+    //     </ScrollableContainer>
+    //   </PageLayout>
+    // </QueryClientProvider>
   );
 };
 
