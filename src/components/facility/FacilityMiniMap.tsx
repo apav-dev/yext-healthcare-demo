3e714b6a6850ae715b6351b95a91d4f2316f3b59
DJ Corbett <dcorbett@yext.com>
2023-08-04 14:13:47 -0500

    cleaning up unused code

--- src/components/facility/FacilityMiniMap.tsx
+++ src/components/facility/FacilityMiniMap.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import AppleMap from "../AppleMap";
 
 const FacilityMiniMap = ({ locations }) => {
--- src/templates/facility.tsx
+++ src/templates/facility.tsx
@@ -49,31 +49,31 @@ export const getHeadConfig: GetHeadConfig<TemplateRenderProps> = ({
   };
 };
 
-const queryClient = new QueryClient();
+// const queryClient = new QueryClient();
 
 const Facility: Template<TemplateRenderProps> = ({
   document,
 }: TemplateProps) => {
-  console.log("document", document);
+  // console.log("document", document);
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <PageLayout
-        featuredLocations={document._site.c_featuredLocations}
-        featuredSpecialties={document._site.c_featuredSpecialties}
-      >
-        <Hero
-          name={document.name}
-          coordinates={document.yextDisplayCoordinate}
-          address={document.address}
-          phone={document.mainPhone}
-        />
-        <FacilityContent
-          description={document.c_facilityDescription}
-          specialties={document.taxonomy_relatedSpecialties}
-        />
-      </PageLayout>
-    </QueryClientProvider>
+    // <QueryClientProvider client={queryClient}>
+    <PageLayout
+      featuredLocations={document._site.c_featuredLocations}
+      featuredSpecialties={document._site.c_featuredSpecialties}
+    >
+      <Hero
+        name={document.name}
+        coordinates={document.yextDisplayCoordinate}
+        address={document.address}
+        phone={document.mainPhone}
+      />
+      <FacilityContent
+        description={document.c_facilityDescription}
+        specialties={document.taxonomy_relatedSpecialties}
+      />
+    </PageLayout>
+    // </QueryClientProvider>
   );
 };
 
