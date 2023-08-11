576b0caafe9635b492397febd2068f23acda64fc
Adrienne Williams <adwilliams@yext.com>
2023-08-11 16:42:32 -0400

    update blog body text

--- src/components/blogs/BlogContent.tsx
+++ src/components/blogs/BlogContent.tsx
@@ -1,12 +1,13 @@
 import { Markdown } from "@yext/react-components";
 import "../../../src/index.css";
 import "../../styles/markdown.css";
+import BodyText from "../atoms/BodyText";
 
 export default function BlogContent({ content }) {
   return (
     <div className="w-full whitespace-pre-wrap px-12 py-12 m-4">
       <div className="w-[88px] h-3 bg-green-700 mb-6 prose" />
-      <Markdown content={content} />
+      <BodyText text={content} />
     </div>
   );
 }
