ccf481fde36967c28dd0e2581678b4eec61ac718
DJ Corbett <dcorbett@yext.com>
2023-08-13 21:20:57 -0400

    article links on specialties

--- src/components/specialty/ArticlesSection.tsx
+++ src/components/specialty/ArticlesSection.tsx
@@ -1,19 +1,22 @@
 import Button from "../facility/Button";
 
-function ArticleCard({ title, imgUrl }) {
+function ArticleCard({ title, imgUrl, url }) {
   return (
     <div className="grow shrink basis-0 self-stretch flex-col justify-between items-start inline-flex">
       <img className="self-stretch h-[262px] object-cover" src={imgUrl} />
       <div className="self-stretch grow shrink basis-0 py-6 flex-col justify-start items-start gap-2.5 flex">
         <div className="self-stretch grow shrink basis-0 flex-col justify-between items-start gap-6 flex">
           <a
-            href="#"
+            href={`/${url}`}
             className="self-stretch grow shrink basis-0 text-zinc-900 text-2xl font-normal leading-[33.99px] hover:underline hover:cursor-pointer"
           >
             {title}
           </a>
           <div className="pb-2 justify-start items-start gap-3 inline-flex">
-            <a className="grow shrink basis-0 text-zinc-900 text-base font-bold leading-normal hover:text-neutral-500 hover:cursor-pointer">
+            <a
+              href={`/${url}`}
+              className="grow shrink basis-0 text-zinc-900 text-base font-bold leading-normal hover:text-neutral-500 hover:cursor-pointer"
+            >
               See More
             </a>
           </div>
@@ -34,6 +37,7 @@ export default function ArticlesSection({ articles }) {
             title={article.name}
             imgUrl={article.photoGallery[0].image.url}
             key={article.id}
+            url={article.slug}
           />
         ))}
       </div>
