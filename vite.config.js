d718cab46be2f35489aaabf451386adaf124676e
dlee <dlee@yext.com>
2023-08-31 15:33:28 +0000

    onemore

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import yextSSG from "@yext/pages/vite-plugin";
 
-export default defineConfig({
-  plugins: [react(), yextSSG()],
-});
+export default defineConfig(({ command, mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd(), '');
+  console.log(env);
+  return {
+    // vite config
+    define: {
+      YEXT_PUBLIC_SEARCH_API_KEY: JSON.stringify(env.YEXT_PUBLIC_SEARCH_API_KEY),
+    },
+    plugins: [react(), yextSSG()],
+  }
+})
