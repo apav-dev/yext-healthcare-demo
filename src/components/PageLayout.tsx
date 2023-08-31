f8b97f1d1e8f6ded619a53cfcb7848b9bd0bb285
dlee <dlee@yext.com>
2023-08-31 15:44:28 +0000

    final2

--- src/components/PageLayout.tsx
+++ src/components/PageLayout.tsx
@@ -12,7 +12,7 @@ export interface LayoutProps {
 }
 
 const searcher = provideHeadless({
-  apiKey: YEXT_PUBLIC_SEARCH_API_KEY,
+  apiKey: __YEXT_PUBLIC_SEARCH_API_KEY__,
   experienceKey: "find-a-doc-specialties",
   locale: "en",
   verticalKey: "healthcare_professionals",
--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,8 @@ import yextSSG from "@yext/pages/vite-plugin";
 
 export default defineConfig(({ command, mode }) => {
   return {
-    // vite config
     define: {
-      YEXT_PUBLIC_SEARCH_API_KEY: JSON.stringify(process.env.YEXT_PUBLIC_SEARCH_API_KEY),
+      __YEXT_PUBLIC_SEARCH_API_KEY__: JSON.stringify(process.env.YEXT_PUBLIC_SEARCH_API_KEY),
     },
     plugins: [react(), yextSSG()],
   }
